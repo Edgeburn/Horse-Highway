9d43a72e84c953e8f1ac69835955f00de6a7822a
Edgeburn <edgeburn02@mailredirect.edgeburnmedia.com>
2023-06-08 01:46:27 -0400

    check player vehicle type is horse when checking it is not null. fixes #13

--- src/main/java/com/edgeburnmedia/horsehighway/HorseHighwayListeners.java
+++ src/main/java/com/edgeburnmedia/horsehighway/HorseHighwayListeners.java
@@ -1,7 +1,6 @@
 package com.edgeburnmedia.horsehighway;
 
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -40,13 +39,10 @@ public class HorseHighwayListeners implements Listener {
 			speedMaterial = playerStandingOn;
 		}
 
-		if (playerVehicle != null) { // first we want to check that the player's vehicle isn't null, and if it is we
-			// just want to ignore and do nothing further
+		if (playerVehicle != null && playerVehicle.getType().equals(EntityType.HORSE)) { // first we want to check that the player's vehicle isn't null, and if it is we just want to ignore and do nothing further
 			if (plugin.getHorseManagers().containsKey(playerVehicle)) {
-				if (playerVehicle.getType() == EntityType.HORSE) { // now that we know it is not null, we can check if it's a horse
-					plugin.getHorseManagers().get((Horse) playerVehicle).updateSpeed(speedMaterial); // tell the horse manager to update the speed with the block the player is currently standing on
-					Speedometer.displaySpeedometer(event, plugin);
-				}
+				plugin.getHorseManagers().get((Horse) playerVehicle).updateSpeed(speedMaterial); // tell the horse manager to update the speed with the block the player is currently standing on
+				Speedometer.displaySpeedometer(event, plugin);
 			} else {
 				plugin.registerHorse((Horse) playerVehicle, event.getPlayer());
 			}
@@ -70,7 +66,7 @@ public class HorseHighwayListeners implements Listener {
 	public void onPlayerDismount(EntityDismountEvent event) {
 		if (
 			event.getEntity() instanceof Player rider &&
-			event.getDismounted().getType().equals(EntityType.HORSE)
+				event.getDismounted().getType().equals(EntityType.HORSE)
 		) {
 			plugin.deregisterHorse(rider, event.getDismounted());
 		}
